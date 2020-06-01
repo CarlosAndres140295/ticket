e4853497b477f87c01ee2c70921cd2d0a46ff673
Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2020-06-01 18:47:33 -0500

    master

--- main/index.php
+++ main/index.php
@@ -10,58 +10,17 @@
 		<link rel="stylesheet" type="text/css" href="../recursos/fontawesome/css/all.min.css">
 </head>
 	<body class="bg-light">
-	 <div class="container-fluid">
-		<div class="">
+	 <div class="container-fluid bg-dark">
 		   <section id="menu">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top ">
-			  <a class="navbar-brand" href="index.php"><img src="../images/img.jpg" class="rounded-circle" style="width: 50px;height: 50px;"></a>
-			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			    <span class="navbar-toggler-icon"></span>
-			  </button>
-			  <div class="collapse navbar-collapse" id="navbarNav">
-			    <ul class="navbar-nav">
-			      <li class="nav-item active">
-			        <a class="nav-link" href="#">Tablero<span class="sr-only">(current)</span></a>
-			      </li>
-			      <li class="nav-item dropdown">
-			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuCliente" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Clientes</a>
-			          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuCliente">
-				          <a class="dropdown-item" href="#">Agregar nuevo cliente <span><i class="fa fa-user-plus"></i></span></a>
-				          <a class="dropdown-item" href="#">Ver listado de clientes <span><i class="fa fa-users"></i></span></a>
-				          <!-- <a class="dropdown-item" href="#">Something else here</a> -->
-			        </div>
-			      </li>
-			      <li class="nav-item">
-			        <a class="nav-link" href="#">Servicios</a>
-			      </li>
-			      <li class="nav-item float-right">
-			        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Manteniento</a>
-			      </li>
-			    <!-- </ul> -->
-			  <!-- </div> -->
-
-			  <!-- <div class="collapse navbar-collapse" id="navbarNav"> -->
-			    <!-- <ul class="navbar-nav"> -->
-			       <li class="nav-item dropdown">
-			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuPerfil" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Usuario
-			        </a>
-			        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuPerfil">
-			          <a class="dropdown-item" href="#">Cuenta <span><i class="fa fa-user"></i></span></a>
-			          <a class="dropdown-item" href="#">Contraseña <span><i class="fa fa-key"></i></span></a>
-			          <a class="dropdown-item" href="#">Salir <span><i class="fa fa-power-off"></i></span></a>
-			        </div>
-			      </li>
-			    </ul>
-			  </div>
-			</nav>
+			<?php include 'menu.php'; ?>
 		</section>
-		</div>
+
      </div>
 
      <div class="container-fluid">
 		<!-- <div class="container"> -->
 			<div id="respuestas">
+				<br>
 				<?php include 'indicadores/indicadores.php' ?>
 			</div>
 		<!-- </div> -->
---
+++ main/menu.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top ">
+			  <a class="navbar-brand" href="index.php">
+				  <img src="../images/img.jpg" class="rounded-circle" style="width: 30px;height: 30px;"></a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarNav">
+			    <ul class="navbar-nav">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="#">Tablero<span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuCliente" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Clientes</a>
+			          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuCliente">
+				          <a class="dropdown-item" href="#">Agregar nuevo cliente <span><i class="fa fa-user-plus"></i></span></a>
+				          <a class="dropdown-item" href="#">Ver listado de clientes <span><i class="fa fa-users"></i></span></a>
+				          <!-- <a class="dropdown-item" href="#">Something else here</a> -->
+			        </div>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Servicios</a>
+			      </li>
+			      <li class="nav-item float-right">
+			        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Manteniento</a>
+			      </li>
+			    <!-- </ul> -->
+			  <!-- </div> -->
+
+			  <!-- <div class="collapse navbar-collapse" id="navbarNav"> -->
+			    <!-- <ul class="navbar-nav"> -->
+			       <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuPerfil" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Usuario
+			        </a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuPerfil">
+			          <a class="dropdown-item" href="#">Cuenta <span><i class="fa fa-user"></i></span></a>
+			          <a class="dropdown-item" href="#">Contraseña <span><i class="fa fa-key"></i></span></a>
+			          <a class="dropdown-item" href="#">Salir <span><i class="fa fa-power-off"></i></span></a>
+			        </div>
+			      </li>
+			    </ul>
+			  </div>
+			</nav>
