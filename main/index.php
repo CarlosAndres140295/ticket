7dc899da6b38dfc2f742b00c1aafc05a01dca373
Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2020-06-01 18:58:17 -0500

    Cambios Menu

--- main/index.php
+++ main/index.php
@@ -30,5 +30,6 @@
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 		<script src="../js/bootstrap.min.js"></script>
 		<script src="../recursos/alertify/alertify.min.js"></script>
+		<script src="js/app_menu.js"></script>
 
 </body>
